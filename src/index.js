491f9027e6f80a1fa335129bb0af1ce00e2ee878
Ivan Maidana <ivan.maidana9912@gmail.com>
2023-02-21 12:45:50 -0300

    repaired

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import express from 'express'
 import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
 import indexRoutes from './routes/index.js'
+import logger from 'morgan'
+const port = 3000 
 
 const app = express()
+app.use(logger('dev'))
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 //console.log(join(__dirname, 'views'))
@@ -15,5 +18,5 @@ app.use(indexRoutes)
 
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(process.env.PORT || 3000)
-console.log('Server is listening on port', process.env.PORT || 3000)
+app.listen(process.env.PORT || port)
+console.log('Server is listening on port', process.env.PORT || port)
