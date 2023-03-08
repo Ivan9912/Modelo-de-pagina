2287487de09619f17d63b7b863764f23ddbac86f
Ivan Maidana <107094517+Ivan9912@users.noreply.github.com>
2023-03-08 17:13:59 -0300

    change of port

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import express from 'express'
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
-import indexRoutes from './routes/routes.js'
-import logger from 'morgan'
-const port = 3000 
+import express from 'express';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import indexRoutes from './routes/routes.js';
+import logger from 'morgan';
+const port = 9912; 
 
-const app = express()
-app.use(logger('dev'))
+const app = express();
+app.use(logger('dev'));
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 //console.log(join(__dirname, 'views'))
 
-app.set('views', join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(indexRoutes)
+app.use(indexRoutes);
 
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')));
 
-app.listen(process.env.PORT || port)
-console.log('Server is listening on port', process.env.PORT || port)
+app.listen(process.env.PORT || port);
+console.log('Server is listening on port', process.env.PORT || port);
